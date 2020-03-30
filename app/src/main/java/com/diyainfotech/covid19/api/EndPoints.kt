28ab8991a0633e67be4f5dadfb1ea2836cac60fb
Sanjay Patel <sanjaybpatel27@gmail.com>
2020-03-29 22:50:59 -0400

    World tracker (#10)
    * create api call to fetch world data for covid 19
    create adaptor for country wise and area wise UI and use as a Nested So we can go As many Deep Levels.
    and show data for whole world
    
    * add world wide numbers to world fragment
    
    Co-authored-by: Sanjay Patel <sanjay@smooth.tech>

--- app/src/main/java/com/diyainfotech/covid19/api/EndPoints.kt
+++ app/src/main/java/com/diyainfotech/covid19/api/EndPoints.kt
@@ -5,4 +5,7 @@ object EndPoints {
     const val INDIA_ALL_DATA  = COVID19_INDIA_BASE_URL + "data.json"
     const val INDIA_STATE_DISTRICT_WISE_DATA  = COVID19_INDIA_BASE_URL + "state_district_wise.json"
 
+    const val COVID19_WORLD_BASE_URL ="https://bing.com/covid/"
+    const val WORLD_ALL_DATA  = COVID19_WORLD_BASE_URL + "data"
+
 }
---
+++ app/src/main/java/com/diyainfotech/covid19/ui/world/WorldServiceManager.kt
@@ -0,0 +1,7 @@
+package com.diyainfotech.covid19.ui.world
+
+import com.diyainfotech.covid19.api.world.WorldData
+
+object WorldServiceManager {
+    var worldData: WorldData? = null
+}
