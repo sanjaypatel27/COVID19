c34a27a3d581b89337838107d999af56262b56a3
Sanjay Patel <sanjay@smooth.tech>
2020-04-11 17:01:52 -0400

    #8 add swipe to refresh layout into India fragment to make api call again and reload data

--- app/src/main/java/com/diyainfotech/covid19/ui/home/HomeFragment.kt
+++ app/src/main/java/com/diyainfotech/covid19/ui/home/HomeFragment.kt
@@ -4,47 +4,76 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
 import com.diyainfotech.covid19.R
 import com.diyainfotech.covid19.api.india.IndiaData
-import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment()  {
+class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by viewModels()
     private lateinit var stateWiseDataIndiaAdaptor: StateWiseDataIndiaAdaptor
     private val linearLayoutManager: LinearLayoutManager = LinearLayoutManager(context)
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var indiaStateWiseDataRecyclerview: RecyclerView
+    private lateinit var lastUpdateTime: TextView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        indiaStateWiseDataRecyclerview = view.findViewById(R.id.indiaStateWiseDataRecyclerview)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        indiaStateWiseDataRecyclerview.layoutManager = linearLayoutManager
+
+        lastUpdateTime = view.findViewById(R.id.lastUpdateTime)
+        swipeRefresh = view.findViewById(R.id.swipeRefresh)
+        swipeRefresh.setColorSchemeResources(R.color.colorAccent)
         subscribers()
+        swipeRefresh.setOnRefreshListener(OnRefreshListener
+        {
+            loadData(true)
+        })
+    }
+
+    private fun loadData(isReload: Boolean) {
+        swipeRefresh.isRefreshing = true
+        homeViewModel.getIndiaAllData()
+        homeViewModel.getAllStateDataAsync()
+        if (isReload) {
+            stateWiseDataIndiaAdaptor.stateList = emptyList()
+            stateWiseDataIndiaAdaptor.notifyDataSetChanged()
+        }
     }
 
     private fun subscribers() {
-        homeViewModel.getIndiaAllData()
-        homeViewModel.getAllStateDataAsync()
-        homeViewModel.indiaData.observe(this, Observer {
+        loadData(false)
+        homeViewModel.indiaData.observe(viewLifecycleOwner, Observer {
+            swipeRefresh.isRefreshing = false
             setupRecyclerview(it!!)
         })
-        homeViewModel.stateDistrictWise.observe(this, Observer {
+        homeViewModel.stateDistrictWise.observe(viewLifecycleOwner, Observer {
             stateWiseDataIndiaAdaptor.notifyDataSetChanged()
         })
     }
 
     private fun setupRecyclerview(indiaData: IndiaData) {
-        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
-        indiaStateWiseDataRecyclerview.layoutManager = linearLayoutManager
         stateWiseDataIndiaAdaptor = StateWiseDataIndiaAdaptor()
-        stateWiseDataIndiaAdaptor.stateList = indiaData.stateList.sortedByDescending { it.confirmed.toInt() }
+        stateWiseDataIndiaAdaptor.stateList =
+            indiaData.stateList.sortedByDescending { it.confirmed.toInt() }
         indiaStateWiseDataRecyclerview.adapter = stateWiseDataIndiaAdaptor
-        lastUpdateTime?.text = indiaData.stateList[0].lastupdatedtime
-
+        lastUpdateTime.text = indiaData.stateList[0].lastupdatedtime
     }
 }
