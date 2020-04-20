74ecb4eb0b313678d546b26b03102f9460223281
Sanjay Patel <sanjay@smooth.tech>
2020-04-19 22:47:21 -0400

    #48 apply View binding on Home Fragment and State Wise Adaptor and View Holder

--- app/src/main/java/com/diyainfotech/covid19/ui/home/IndiaDataServiceManager.kt
+++ app/src/main/java/com/diyainfotech/covid19/ui/home/IndiaDataServiceManager.kt
@@ -6,13 +6,18 @@ object IndiaDataServiceManager {
     var indiaData: IndiaData? = null
     var stateDistrictWise: StateWiseDataResponseV2? = null
 
-    private fun getDistrictDataBasedOnString(stateName: String): State {
-        val state = stateDistrictWise!!.filter { it.state.equals(stateName ,true)}
-        return state[0]
+    private fun getDistrictDataBasedOnString(stateName: String): State? {
+        val state = stateDistrictWise!!.filter { it.state.equals(stateName, true) }
+        return if (state.isNotEmpty()) {
+            state[0]
+        } else {
+            null
+        }
+
     }
 
     fun getDistrictListFromDistrictData(stateName: String): List<District> {
         val state = getDistrictDataBasedOnString(stateName)
-        return state.districtList
+        return state?.districtList ?: ArrayList()
     }
 }
